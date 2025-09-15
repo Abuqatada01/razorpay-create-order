25fea09b0fde998d7360373de13b8987b3858d70
Abuqatada01 <sabuqatada@gmail.com>
2025-09-15 23:31:44 +0530

    remove .env

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ const { Client, Databases, ID } = require('node-appwrite');
         }
 
         // Env validation
-        const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
-        const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
-        const APPWRITE_ENDPOINT = process.env.APPWRITE_FUNCTION_ENDPOINT || process.env.APPWRITE_ENDPOINT;
-        const APPWRITE_PROJECT = process.env.APPWRITE_FUNCTION_PROJECT_ID || process.env.APPWRITE_PROJECT_ID;
-        const APPWRITE_API_KEY = process.env.APPWRITE_FUNCTION_API_KEY || process.env.APPWRITE_API_KEY;
-        const DATABASE_ID = process.env.APPWRITE_DATABASE_ID;
-        const ORDERS_COLLECTION_ID = process.env.APPWRITE_ORDERS_COLLECTION_ID;
+        const RAZORPAY_KEY_ID = "rzp_test_RH8HdkZbA9xnoK";
+        const RAZORPAY_KEY_SECRET = "V2OIX2UM8B6CGlxk0UjzQmk1";
+        const APPWRITE_ENDPOINT = "https://fra.cloud.appwrite.io/v1"
+        const APPWRITE_PROJECT = "684c05fe002863accd73"
+        const APPWRITE_API_KEY = 'standard_62d893d06bb23cce71d8bdb735be5e2f7b1a98e3d0927e78f52b1abfce08d2adbd47b712c335638fb59bf94b748b62184e577c115a2caf53b2342e779dd188fa39f3c3d105e409e6554a3c2b6341dc87728ff60a4ff540d350e3bb181d313dbae8f8de3a994ab753095e4d2b18fe57b033f7234acbdcbe2ba630c155d06b9558'
+        const DATABASE_ID = "68c414290032f31187eb"
+        const ORDERS_COLLECTION_ID = "68c58bfe0001e9581bd4";
 
         if (!RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET) {
             console.log(JSON.stringify({ success: false, message: 'missing razorpay keys' }));
